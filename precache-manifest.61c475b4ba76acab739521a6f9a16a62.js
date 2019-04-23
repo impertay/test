5321dd7d80337cd99c3490d0598604c2c1cbd5c0
Artem Mikhaylov <impertay@yandex.ru>
2019-04-23 11:58:58 +0300

    Updates

---
+++ precache-manifest.61c475b4ba76acab739521a6f9a16a62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/test/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "2160d828785e96a67ccd",
+    "url": "/test/static/js/runtime~main.eded8796.js"
+  },
+  {
+    "revision": "6f40b991448d6fd10864",
+    "url": "/test/static/js/main.15b6f055.chunk.js"
+  },
+  {
+    "revision": "35dee558b4ebf62459ee",
+    "url": "/test/static/js/2.23a9eeab.chunk.js"
+  },
+  {
+    "revision": "6f40b991448d6fd10864",
+    "url": "/test/static/css/main.2bcd4f9c.chunk.css"
+  },
+  {
+    "revision": "35dee558b4ebf62459ee",
+    "url": "/test/static/css/2.d02486b8.chunk.css"
+  },
+  {
+    "revision": "3d101c475fe19164ee46513948061f59",
+    "url": "/test/index.html"
+  }
+];
